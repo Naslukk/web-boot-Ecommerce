aadbf7d1be1a11d2e102b5c2b4f5cfc5b6d7271f
Naslu kk <naslukk@gmail.com>
2023-09-18 19:39:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// setInterval(function(){
+//     var current = $('.active');
+//     var next = current.next();
+//     if (next.length) {
+//         current.removeClass('active');
+//         next.addClass('active')
+//     }
+//     else{
+//         current.removeClass('active')
+//         $('.heading:first-child').addClass('.active')
+//     }
+// }, 4000)
